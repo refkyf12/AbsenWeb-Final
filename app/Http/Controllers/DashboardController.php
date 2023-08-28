25468832765a0bd25bdd7c5695d8d88d7074b248
fajri2002 <fajri2002@gmail.com>
2023-08-28 11:55:36 +0700

    dashboard harian mingguan wip

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -16,9 +16,12 @@ class DashboardController extends Controller
         // $result=DB::select(DB::raw("select count(*) as total, keterlambatan from log_absen group by keterlambatan"));
         // dd($result);
         $lateDailyLog = $this->getLateDailyLog();
-        return view('dashboard', compact('lateDailyLog'));
+        $absenceLog = $this->getAbsenceLog();
+        $lateWeeklyLog = $this->getLateWeeklyLog();
+        $lateMonthlyLog = $this->getLateMonthlyLog();
+        return view('dashboard', compact('lateDailyLog', 'absenceLog', 'lateWeeklyLog'));
     }
-
+ 
     function getLateDailyLog()
      {
         $today = date('Y-m-d');
@@ -36,6 +39,43 @@ class DashboardController extends Controller
 
     }
 
+    function getLateWeeklyLog()
+     {
+        $today = date('Y-m-d');
+        $week = date('Y-m-d', strtotime('-7 days'));
+        $query = logAbsen::select('jam_masuk', 'nama')->join('users', 'users.id', 'log_absen.users_id' )
+                ->where('keterlambatan', '=', '1')
+                ->whereBetween('tanggal', [$week, $today]);
+        $list = $query -> get();
+        $count = $query -> count();
+        $data = [
+            'list'=> $list,
+            'total'=> $count
+        ];
+
+        return $data;
+
+    }
+
+    function getLateMonthlyLog()
+     {
+        $today = date('Y-m-d');
+        $week = date('Y-m-d', strtotime('-30 days'));
+        $query = logAbsen::select('jam_masuk', 'nama')->join('users', 'users.id', 'log_absen.users_id' )
+                ->where('keterlambatan', '=', '1')
+                ->whereBetween('tanggal', [$week, $today]);
+        $list = $query -> get();
+        $count = $query -> count();
+        $data = [
+            'list'=> $list,
+            'total'=> $count
+        ];
+        return $data;
+
+    }
+
+
+
     function getAbsenceLog() 
     {
         $today = date('Y-m-d');
@@ -50,4 +90,37 @@ class DashboardController extends Controller
 
         return $data;
     }
+
+    function getWeeklyAbsenceLog() 
+    {
+        $today = date('Y-m-d');
+        $week = date('Y-m-d', strtotime('-7 days'));
+        $query = Ketidakhadiran::select('tanggal', 'nama')->join('users', 'users.id', 'ketidakhadiran.users_id' )
+                ->where('tanggal', '=', '2023-07-03');
+        $list = $query -> get();
+        $count = $query -> count();
+        $data = [
+            'list'=> $list,
+            'total'=> $count
+        ];
+
+        return $data;
+    }
+
+    function getMonthlyAbsenceLog() 
+    {
+        $today = date('Y-m-d');
+        $query = Ketidakhadiran::select('tanggal', 'nama')->join('users', 'users.id', 'ketidakhadiran.users_id' )
+                ->where('tanggal', '=', '2023-07-03');
+        $list = $query -> get();
+        $count = $query -> count();
+        $data = [
+            'list'=> $list,
+            'total'=> $count
+        ];
+
+        return $data;
+    }
+    
+
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,84 +5,166 @@
 
 
 <!-- Modal -->
-<div class="modal fade" id="modalTerlambat" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <div class="col-12">
-          <div class="table-responsive">
-            <table class="table table-hover">
-              <thead>
-                <tr>
-                  <th>Jam Masuk</th>
-                  <th>Nama</th>
-                </tr>
-              </thead>
-              <tbody>
-                @foreach($lateDailyLog['list'] as $e=>$dt)
-                <tr>
-                  <td>{{$dt->jam_masuk}}</td>
-                  <td>{{$dt->nama}}</td>
-                </tr>
-                @endforeach
-              </tbody>
-            </table>
-          </div>
+<div class="modal fade" id="modalTerlambat" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Data Terlambat Harian</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="col-12">
+                    <div class="table-responsive">
+                        <table class="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th>Jam Masuk</th>
+                                    <th>Nama</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($lateDailyLog['list'] as $e=>$dt)
+                                <tr>
+                                    <td>{{$dt->jam_masuk}}</td>
+                                    <td>{{$dt->nama}}</td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
         </div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-      </div>
     </div>
-  </div>
+</div>
+
+<div class="modal fade" id="modalAbsen" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Data Absen Harian</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="col-12">
+                    <div class="table-responsive">
+                        <table class="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th>Jam Masuk</th>
+                                    <th>Nama</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($absenceLog['list'] as $e=>$dt)
+                                <tr>
+                                    <td>{{$dt->tanggal}}</td>
+                                    <td>{{$dt->nama}}</td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
 </div>
 
 <div class="col-12 col-sm-6 col-md-4">
-  <div class="info-box mb-3" data-toggle="modal" data-target="#modalTerlambat">
-    <span class="info-box-icon bg-danger elevation-1"><i class="fa fa-user"></i></span>
+    <div class="info-box mb-3" data-toggle="modal" data-target="#modalTerlambat">
+        <span class="info-box-icon bg-danger elevation-1"><i class="fa fa-user"></i></span>
 
-    <div class="info-box-content">
-      <span class="info-box-text">Karyawan Terlambat</span>
-      <span class="info-box-number">{{$lateDailyLog['total']}}</span>
+        <div class="info-box-content">
+            <span class="info-box-text">Karyawan Terlambat</span>
+            <span class="info-box-number">{{$lateDailyLog['total']}}</span>
+        </div>
+        <!-- /.info-box-content -->
     </div>
-    <!-- /.info-box-content -->
-  </div>
-  <!-- /.info-box -->
+    <!-- /.info-box -->
 </div>
 
 <div class="col-12 col-sm-6 col-md-4">
-  <div class="info-box mb-3">
-    <span class="info-box-icon bg-danger elevation-1"><i class="fa fa-user"></i></span>
+    <div class="info-box mb-3" data-toggle="modal" data-target="#modalAbsen">
+        <span class="info-box-icon bg-danger elevation-1"><i class="fa fa-user"></i></span>
 
-    <div class="info-box-content">
-      <span class="info-box-text">Karyawan Tidak Hadir</span>
-      <span class="info-box-number">41,410</span>
+        <div class="info-box-content">
+            <span class="info-box-text">Karyawan Tidak Masuk</span>
+            <span class="info-box-number">{{$absenceLog['total']}}</span>
+        </div>
+        <!-- /.info-box-content -->
     </div>
-    <!-- /.info-box-content -->
-  </div>
-  <!-- /.info-box -->
+    <!-- /.info-box -->
 </div>
 
 <div class="col-12 col-sm-6 col-md-4">
-  <div class="info-box mb-3">
-    <span class="info-box-icon bg-danger elevation-1"><i class="fa fa-user"></i></span>
+    <div class="info-box mb-3">
+        <span class="info-box-icon bg-danger elevation-1"><i class="fa fa-user"></i></span>
 
-    <div class="info-box-content">
-      <span class="info-box-text">Karyawan</span>
-      <span class="info-box-number">41,410</span>
+        <div class="info-box-content">
+            <span class="info-box-text">Karyawan Cuti</span>
+            <span class="info-box-number">41,410</span>
+        </div>
+        <!-- /.info-box-content -->
     </div>
-    <!-- /.info-box-content -->
-  </div>
-  <!-- /.info-box -->
+    <!-- /.info-box -->
 </div>
 
-<div class="chart tab-pane" id="sales-chart" style="position: relative; height: 300px;"></div>
+<div class="chart tab-pane" id="daily-chart" style="position: relative; height: 300px;"></div>
+
+<div class="col-12 col-sm-6 col-md-4">
+    <div class="info-box mb-3" data-toggle="modal" data-target="#modalTerlambat">
+        <span class="info-box-icon bg-danger elevation-1"><i class="fa fa-user"></i></span>
+
+        <div class="info-box-content">
+            <span class="info-box-text">Karyawan Terlambat</span>
+            <span class="info-box-number">{{$lateWeeklyLog['total']}}</span>
+        </div>
+        <!-- /.info-box-content -->
+    </div>
+    <!-- /.info-box -->
+</div>
+
+<div class="col-12 col-sm-6 col-md-4">
+    <div class="info-box mb-3">
+        <span class="info-box-icon bg-danger elevation-1"><i class="fa fa-user"></i></span>
+
+        <div class="info-box-content">
+            <span class="info-box-text">Karyawan Cuti</span>
+            <span class="info-box-number">41,410</span>
+        </div>
+        <!-- /.info-box-content -->
+    </div>
+    <!-- /.info-box -->
+</div>
+
+<div class="col-12 col-sm-6 col-md-4">
+    <div class="info-box mb-3">
+        <span class="info-box-icon bg-danger elevation-1"><i class="fa fa-user"></i></span>
+
+        <div class="info-box-content">
+            <span class="info-box-text">Karyawan Cuti</span>
+            <span class="info-box-number">41,410</span>
+        </div>
+        <!-- /.info-box-content -->
+    </div>
+    <!-- /.info-box -->
+</div>
+
+<div class="chart tab-pane" id="week-chart" style="position: relative; height: 300px;"></div>
+
 
 <!-- <div class="col-12 col-sm-6 col-md-4">
   <div class="table-responsive">
@@ -108,19 +190,49 @@
 @endsection
 @section('scripts')
 <script>
-  var karyawanTerlambat = "{{$lateDailyLog['total']}}"
-  var donut = new Morris.Donut({
-    element  : 'sales-chart',
-    resize   : true,
-    colors   : ['#3c8dbc', '#f56954', '#00a65a'],
-    data     : [
-      { label: 'Karyawan Terlambat', value: karyawanTerlambat},
-      { label: 'Karyawan Tidak Masuk', value: 30 },
-      { label: 'Karyawan Cuti', value: 20 }
-    ],
-    hideHover: 'auto'
-  });
+    var karyawanTerlambat = "{{$lateDailyLog['total']}}"
+    var karyawanAbsen = "{{$absenceLog['total']}}"
+    var donut = new Morris.Donut({
+        element: 'daily-chart',
+        resize: true,
+        colors: ['#3c8dbc', '#f56954', '#00a65a'],
+        data: [{
+                label: 'Karyawan Terlambat',
+                value: karyawanTerlambat
+            },
+            {
+                label: 'Karyawan Tidak Masuk',
+                value: karyawanAbsen
+            },
+            {
+                label: 'Karyawan Cuti',
+                value: 20
+            }
+        ],
+        hideHover: 'auto'
+    });
+    var donut2 = new Morris.Donut({
+        element: 'week-chart',
+        resize: true,
+        colors: ['#3c8dbc', '#f56954', '#00a65a'],
+        data: [{
+                label: 'Karyawan Terlambat',
+                value: karyawanTerlambat
+            },
+            {
+                label: 'Karyawan Tidak Masuk',
+                value: karyawanAbsen
+            },
+            {
+                label: 'Karyawan Cuti',
+                value: 20
+            }
+        ],
+        hideHover: 'auto'
+    });
 
-  donut.redraw(); 
+    donut.redraw();
+    donut2.redraw();
+
 </script>
 @endsection
