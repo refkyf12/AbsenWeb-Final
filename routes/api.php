04e4ae7d4dc2b8ec01c4259a12549b470d7b1be6
Pradeva <90541409+Pradeva@users.noreply.github.com>
2023-08-30 11:38:36 +0700

    ubah api

--- routes/api.php
+++ routes/api.php
@@ -28,6 +28,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 // Route::resource('karyawan', UserController::class);
 Route::post('/authenticate', 'App\Http\Controllers\UserController@authUser');
 Route::post('/cuti/create/store', 'App\Http\Controllers\CutiController@storeMobile');
+Route::get('/libur_nasional', 'App\Http\Controllers\LiburNasionalController@index');
 Route::resource('lebihKerja', LebihKerjaController::class);
 Route::resource('cuti', CutiController::class);
 Route::resource('lembur', LemburController::class);
